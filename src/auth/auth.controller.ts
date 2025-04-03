823a3cc737ca02a3ad82cf7cb4fc8bb72fa00c7b
manuelgardon <--help>
2025-04-03 00:25:10 -0300

    Fixes tipado de autenticación

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ErrorResponse, User } from 'src/utils/types';
+import { ErrorResponse } from 'src/utils/types';
 import { LoginResponse } from './auth.interfaces';
 
 @Controller('auth')
@@ -8,8 +8,8 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('login')
-    async login(@Body() credentials: Partial<User>): Promise<ErrorResponse | LoginResponse> {
-        const user = await this.authService.validateUser(credentials);
+    async login(@Body() body: { username: string, password: string }): Promise<ErrorResponse | LoginResponse> {
+        const user = await this.authService.validateUser(body.username, body.password);
         if (!user) return { statusCode: 403, message: 'Invalid credentials' };
         else return this.authService.login(user);
     } 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { LoginResponse } from './auth.interfaces';
-import { User } from 'src/utils/types';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -11,8 +11,7 @@ export class AuthService {
         private readonly jwtService: JwtService,
     ) {}
 
-    async validateUser({ username, password }: Partial<User>): Promise<Partial<User> | null> {
-        if (!username || !password) return null;
+    async validateUser(username: string, password: string): Promise<Partial<User> | null> {
         const user = await this.usersService.findOne(username);
         if (user && user.password === password) {
             const { password, ...result } = user;
